fbd45e5e1de6167765e13bb2f6c17f5964443ff7
fannyokta <99315908+fannyokta@users.noreply.github.com>
2022-12-25 05:17:29 +0700

    Create AnggotaController.php

---
+++ app/Http/Controllers/AnggotaController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Anggota;
+
+class AnggotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $anggota = Anggota::all();
+        return view('anggota.index', [
+            'anggota' => $anggota
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('anggota.create');
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => ['required', 'string', 'max:200'],
+            'nik' => 'required',
+            'jk' => 'required',
+            'tmp_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'status' => 'required',
+            'agama' => 'required',
+            'pekerjaan' => 'required',
+            'alamat' => 'required',
+            'kota' => 'required',
+            'no_telp' => 'required',
+            'tgl_daftar' => 'required',
+            'aktif' => 'required',
+            'npa' => 'required',
+            'no_akun' => 'required',
+            'bank' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        Anggota::create($request->all());
+
+        return redirect(route('anggota.index'))->with('success', 'Data Berhasil Disimpan');
+    }
+
+    public function show($id)
+    {
+        return view('anggota.show',compact('anggota'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('anggota.edit', compact('sisw'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Anggota $anggota)
+    {
+        $request->validate([
+            'nama' => ['required', 'string', 'max:200'],
+            'nik' => 'required',
+            'jk' => 'required',
+            'tmp_lahir' => 'required',
+            'tgl_lahir' => 'required',
+            'status' => 'required',
+            'agama' => 'required',
+            'pekerjaan' => 'required',
+            'alamat' => 'required',
+            'kota' => 'required',
+            'no_telp' => 'required',
+            'tgl_daftar' => 'required',
+            'aktif' => 'required',
+            'npa' => 'required',
+            'no_akun' => 'required',
+            'bank' => 'required',
+            'username' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+        $anggota->update($request->all());
+
+        return redirect()->route('anggota.index')
+            ->with('success_message', 'Berhasil mengubah anggota');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Anggota $anggota)
+    {
+        $anggota->delete();
+
+        return redirect()->route('anggota.index')->with('succes','Anggota Berhasil di Hapus');
+    }
+}
