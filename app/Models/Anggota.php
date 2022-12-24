9ff85bf755efa03d6bab8dba7eb93371f6c331e3
fannyokta <99315908+fannyokta@users.noreply.github.com>
2022-12-25 05:17:43 +0700

    Create Anggota.php

---
+++ app/Models/Anggota.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Anggota extends Model
+{
+    use HasFactory;
+
+    protected $table = 'anggotas';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'nama', 'nik', 'jk','tmp_lahir','tgl_lahir','status','agama','pekerjaan','alamat','kota','no_telp','tgl_daftar','aktif','npa',
+        'no_akun','bank','username','email','password',
+    ];
+}
