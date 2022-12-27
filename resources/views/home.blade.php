a66bbea01b36d7e6cce383e15b0634757ba01e02
balqiz <balqiznurqalbi@students.amikom.ac.id>
2022-12-27 15:38:58 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,13 @@
 {{-- @section('title', 'AdminLTE') --}}
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Beranda</h1>
+<h1 class="m-0 text-dark">Beranda</h1>
 @stop
 
 @section('content')
-    <div class="row">
-        <div class="col-md-4">
+<div class="row">
+
+    <!-- <div class="col-md-4">
             <div class="info-box">
                 <span class="info-box-icon bg-info"><i class="far fa-bookmark"></i></span>
                 <div class="info-box-content">
@@ -22,9 +23,9 @@
                     </span>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="col-md-4">
+    <!-- <div class="col-md-4">
             <div class="info-box bg-gradient-warning">
                 <span class="info-box-icon"><i class="far fa-calendar-alt"></i></span>
                 <div class="info-box-content">
@@ -38,21 +39,98 @@
                     </span>
                 </div>
             </div>
-        </div>
+        </div> -->
 
-        <div class="col-md-4">
-            <div class="small-box bg-gradient-success">
-                <div class="inner">
-                    <h3>44</h3>
-                    <p>User Registrations</p>
-                </div>
-                <div class="icon">
-                    <i class="fas fa-user-plus"></i>
-                </div>
-                <a href="#" class="small-box-footer">
-                    More info <i class="fas fa-arrow-circle-right"></i>
-                </a>
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-user"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data petugas</span>
+                <span class="info-box-number">1,410</span>
             </div>
         </div>
     </div>
+
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-file"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data Simpanan</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="fas fa-fw fa-book"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data Pinjaman</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-user"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data Anggota</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="far fa-bookmark"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data Kategori Simpanan</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4">
+        <div class="info-box">
+            <span class="info-box-icon bg-info"><i class="ffas fa-fw fa-money-bill"></i></span>
+            <div class="info-box-content">
+                <span class="info-box-text">Data Angsuran</span>
+                <span class="info-box-number">1,410</span>
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="col-md-4">
+        <div class="small-box bg-gradient-success">
+            <div class="inner">
+                <h3>Rp30.000.000</h3>
+                <p>Data Pinjaman</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-user-plus"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="small-box bg-gradient-success">
+            <div class="inner">
+                <h3>44</h3>
+                <p>User Registrations</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-user-plus"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+                More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+        </div>
+    </div> -->
+</div>
+</div>
+
+
 @stop
